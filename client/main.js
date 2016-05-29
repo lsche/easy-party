bcab6dbca550a96ea804e4ea8896122c23ea1ab0
Louisa Schermuly <louisaschermuly@gmx.de>
2016-05-29 17:49:23 +0200

    add materialize

--- client/main.js
+++ client/main.js
@@ -1,5 +1,4 @@
 import angular from 'angular';
-import 'bootstrap/dist/css/bootstrap.css';
 import angularMeteor from 'angular-meteor';
  
 import { name as Socially } from '../imports/ui/components/socially/socially';
