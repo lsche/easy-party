6d2add05c9d69b9a4a50def40f78aff44df45b9e
Louisa Schermuly <louisaschermuly@gmx.de>
2016-06-01 12:41:27 +0200

    replace auth with userAuth - add RegisterModal (not showing a form yet)

--- imports/ui/components/userAuth/userAuth.js
+++ imports/ui/components/userAuth/userAuth.js
@@ -4,16 +4,25 @@ import angularMeteor from 'angular-meteor';
 import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 
-import template from './auth.html';
+import template from './userAuth.html';
+import modalRegistrationTemplate from './userRegisterModal.html';
 import { name as DisplayNameFilter } from '../../filters/displayNameFilter';
+//TODO probably something missing here: vgl Line 6 in partyAddButton.js
+//TODO probably something missing here: vgl Line 9-11 in auth.js
+import { name as Login } from '../login/login';
+import { name as Register } from '../register/register';
+import { name as Password } from '../password/password';
 
-const name = 'auth';
+const name = 'userAuth';
 
-class Auth {
-    constructor($scope, $reactive) {
+class UserAuth {
+    constructor($scope, $reactive, $mdDialog, $mdMedia) {
         'ngInject';
 
         $reactive(this).attach($scope);
+
+        this.$mdDialog = $mdDialog;
+        this.$mdMedia = $mdMedia;
 
         this.helpers({
             isLoggedIn() {
@@ -28,14 +37,36 @@ class Auth {
     logout() {
         Accounts.logout();
     }
+    openRegistration(event){
+        this.$mdDialog.show({
+            controller($mdDialog) {
+                'ngInject';
+
+                this.close = () => {
+                    $mdDialog.hide();
+                }
+            },
+            controllerAs: 'userRegisterModal',
+            template: modalRegistrationTemplate,
+            targetEvent: event,
+            parent: angular.element(document.body),
+            clickOutsideToClose: true,
+            fullscreen: this.$mdMedia('sm') || this.$mdMedia('xs')
+        });
+    }
 }
 
 // create a module
 export default angular.module(name, [
     angularMeteor,
-    DisplayNameFilter
+    DisplayNameFilter,
+    Login,
+    Register,
+    Password
+    //TODO: evtl fehlt hier etwas. vgl partyaddButton.js line 40
+    //TODO: evtl fehlt hier etwas. vgl auth.js line 40-43
 ]).component(name, {
     template,
     controllerAs: name,
-    controller: Auth
+    controller: UserAuth
 });
